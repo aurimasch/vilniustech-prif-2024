1471d99dc212b28b9e59cd2d030d0fe80993be4a
Aurimas <>
2024-09-23 19:10:21 +0300

    Strukturinis kodas

--- src/Main.java
+++ src/Main.java
@@ -2,98 +2,112 @@ import java.io.IOException;
 
 public class Main {
 
-    static int[][] map = {
-        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,},
-        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,},
-        {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,},
-        {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,},
-        {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,},
-        {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,},
-        {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,},
-        {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,},
-        {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,},
-        {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1,},
-        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,},
-        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,},
-        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,},
-        {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1,},
-        {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,},
-    } ;
+    class Map {
+        int[][] model = {
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,},
+                {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,},
+                {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,},
+                {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,},
+                {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,},
+                {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,},
+                {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,},
+                {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,},
+                {1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1,},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1,},
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,},
+        } ;
+    }
 
-    static int pacmanX = 1;
-    static int pacmanY = 1;
+    class Pacman {
+        int x = 1;
+        int y = 1;
+    }
 
-    static int ghostX = 5;
-    static int ghostY = 5;
-
-    static int ghostDx = 1;
-    static int ghostDy = 0;
+    class Ghost {
+        int x = 5;
+        int y = 5;
+        int dx = 1;
+        int dy = 0;
+    }
 
 
     public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        main.execute();
+    }
+
+    private void execute() throws IOException {
+        Map map = new Map();
+        Pacman pacman = new Pacman();
+        Ghost ghost = new Ghost();
+
 
         while (true) {
             int n = System.in.read();
             if (n == 10)
                 continue;
 
-            processUserInput(n);
+            processUserInput(n, map, pacman);
 
-            if (map[ghostX +ghostDx][ghostY +ghostDy] !=1) {
-                moveGhost();
+            if (map.model[ghost.x +ghost.y][ghost.y +ghost.dy] !=1) {
+                moveGhost(ghost);
             } else {
-                changeDirection();
+                changeDirection(ghost);
             }
 
-            if (pacmanX == ghostX && pacmanY ==ghostY)
+            if (pacman.x == ghost.x && pacman.y ==ghost.y)
                 System.exit(0);
 
-            renderMap();
+            renderMap(map, pacman, ghost);
         }
-
-
     }
 
-    private static void renderMap() {
-        for (int i=0; i<map.length; i++) {
-            for (int j=0; j<map[i].length; j++)  {
-                if (map[i][j] == 1)
+
+
+    private static void renderMap(Map map, Pacman pacman, Ghost ghost) {
+        for (int i=0; i<map.model.length; i++) {
+            for (int j=0; j<map.model[i].length; j++)  {
+                if (map.model[i][j] == 1)
                     System.out.print("#");
-                if (i == pacmanX && j == pacmanY)
+                if (i == pacman.x && j == pacman.y)
                     System.out.print("C");
-                if (i == ghostX && j == ghostY)
+                if (i == ghost.x && j == ghost.y)
                     System.out.print("G");
 
-                else if (map[i][j] == 0)
+                else if (map.model[i][j] == 0)
                     System.out.print(" ");
             }
             System.out.println();
         }
     }
 
-    private static void changeDirection() {
-        int tmp = ghostDx;
-        ghostDx = -1*ghostDy;
-        ghostDy = tmp;
+    private static void changeDirection(Ghost ghost) {
+        int tmp = ghost.dx;
+        ghost.dx = -1*ghost.dy;
+        ghost.dy = tmp;
     }
 
-    private static void moveGhost() {
-        ghostX = ghostX +ghostDx;
-        ghostY = ghostY +ghostDy;
+    private static void moveGhost(Ghost ghost) {
+        ghost.x =  ghost.x  +ghost.dx;
+        ghost.y = ghost.y + ghost.dy;
     }
 
-    private static void processUserInput(int n) {
+    private static void processUserInput(int n, Map map, Pacman pacman) {
         if (n == 'a')
-            if (map[pacmanX][pacmanY-1] != 1)
-                pacmanY--;
+            if (map.model[pacman.x][pacman.y-1] != 1)
+                pacman.y--;
         if (n == 'd')
-            if (map[pacmanX][pacmanY+1] != 1)
-                pacmanY++;
+            if (map.model[pacman.x][pacman.y+1] != 1)
+                pacman.y++;
         if (n == 'w')
-            if (map[pacmanX-1][pacmanY] != 1)
-                pacmanX--;
+            if (map.model[pacman.x-1][pacman.y] != 1)
+                pacman.x--;
         if (n == 's')
-            if (map[pacmanX+1][pacmanY] != 1)
-                pacmanX++;
+            if (map.model[pacman.x+1][pacman.y] != 1)
+                pacman.x++;
     }
 }

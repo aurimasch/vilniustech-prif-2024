2ae3ba5a22adf511964f7f800775906db2d263a7
ugnius.spokavicius <uspokavicius@gmail.com>
2024-09-30 20:44:08 +0300

    Added functionality to move pacman with arrows

--- src/GameRules.java
+++ src/GameRules.java
@@ -1,3 +1,5 @@
+import java.awt.event.KeyEvent;
+
 public class GameRules {
 
     private Map map;
@@ -10,17 +12,17 @@ public class GameRules {
         this.pacman = pacman;
     }
 
-    public void processUserInput(int n) {
-        if (n == 'a')
+    public void processUserInput(int n, int m) {
+        if (n == 'a' || KeyEvent.VK_LEFT == m)
             if (!map.isWall(pacman.getX(), pacman.getY()-1))
                 pacman.moveLeft();
-        if (n == 'd')
+        if (n == 'd' || KeyEvent.VK_RIGHT == m)
             if (!map.isWall(pacman.getX(), pacman.getY()+1))
                 pacman.moveRight();
-        if (n == 'w')
+        if (n == 'w' || KeyEvent.VK_UP == m)
             if (!map.isWall(pacman.getX()-1, pacman.getY()))
                 pacman.moveUp();
-        if (n == 's')
+        if (n == 's' || KeyEvent.VK_DOWN == m)
             if (!map.isWall(pacman.getX()+1, pacman.getY()))
                 pacman.moveDown();
     }
